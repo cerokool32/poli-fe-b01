5f262e8bda39d443881b4db848b550f1c3686069
Marlon Rodriguez <cerokool32@gmail.com>
2024-09-20 10:07:11 -0500

    medios de pago y contacto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import {LoginComponent} from "./security/login/login.component";
 import {RegisterComponent} from "./security/register/register.component";
 import {AdminComponent} from "./home/admin/admin.component";
 import {UserComponent} from "./home/user/user.component";
+import {PaymentComponent} from "./home/payment/payment.component";
+import {ContactComponent} from "./home/contact/contact.component";
 
 export const routes: Routes = [
   {
@@ -35,6 +37,14 @@ export const routes: Routes = [
       {
         path: 'user',
         component: UserComponent
+      },
+      {
+        path:'payment',
+        component: PaymentComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
       }
     ]
   }
